ca8fb61b1e056c094e7183a7af62add8a1aa4f72
Aman Agrawala <amanagrawala@gmail.com>
2016-01-10 17:28:05 -0500

    Add code to show restaurants and a edit and delete button.

--- restaurantserver.py
+++ restaurantserver.py
@@ -12,6 +12,32 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession() 
 
+class Handler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/restaurants"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				restaurants = session.query(Restaurant).all()
+				output = '<html><body>'
+				output += '<h2><a href = "/restaurants/new">Make a New Restaurant Here</a></h2>'
+
+				for restaurant in restaurants:
+					output += "<h2>%s<h2>" % restaurant.name
+					output += "<ul><li><a href = '/restaurants/%s/edit'>Edit</a></li>" % restaurant.id
+					output += "<li><a href = 'restaurants/%s/delete'>Delete</a></li></ul>" % restaurant.id
+					#print output # for debugging
+
+				output += "</body></html>"
+				self.wfile.write(output) # send output back to client
+				return # exit if statement
+		except IOError:
+			self.send_error(404, "File Not Found: %s" % self.path)
+		
+
+
 
 def main():
 	try:
